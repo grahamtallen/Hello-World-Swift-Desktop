0b8936abeb877d1762785e2de5ebf4a89db24b0a
Graham Allen <grahamallen@Grahams-MacBook-Pro.local>
2018-05-26 21:40:04 -0700

    Initial Commit

---
+++ HelloWorld-1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HelloWorld-1
+//
+//  Created by Graham Allen on 5/26/18.
+//  Copyright © 2018 Graham Allen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HelloWorld-1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorld-1
+//
+//  Created by Graham Allen on 5/26/18.
+//  Copyright © 2018 Graham Allen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
